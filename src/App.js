bbde59b35f1821e0eba204657a1f8c51780795a9
a.demaj <a.demaj@teamystem.com>
2024-03-17 17:48:18 +0100

    build changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Igor Rada 34
+          Gof Kac
         </a>
       </header>
     </div>
